ebd64346d731542e3d38947157ea454e376669a9
Uzair ul Hasan <48287844+Uzzikuzzi@users.noreply.github.com>
2023-03-02 18:29:21 +0500

    Update app.js

--- app.js
+++ app.js
@@ -47,40 +47,40 @@ function openTest(evt, testimonial) {
 }
 
 // intersection observer
-if ('IntersectionObserver' in window)  {
-  // Recommended: make this selector more specific with a `data-animate-on-visible`
-  let elements = document.querySelectorAll("svg");
-
-  let observer = new IntersectionObserver(entries => {
-    // quit early if users wants Reduced Motion
-    let mediaQuery = window.matchMedia('(prefers-reduced-motion: no-preference)');
-    if(!mediaQuery.matches) {
-      return;
-    }
-
-    for(let entry of entries) {
-      if(!entry.isIntersecting) {
-        continue;
-      }
-
-      // Look for <animate> or <animateTransform> that need JS to start
-      let beginElements = entry.target.querySelectorAll(`:scope [begin="indefinite"]`);
-      for(let beginEl of beginElements) {
-        beginEl.beginElement();
-
-        // Unobserve so we don’t re-animate the dead
-        observer.unobserve(entry.target);
-      }
-    }
-  },
-  {
-    threshold: .5 // 50% of element must be visible
-  });
-
-  for(let elem of elements) {
-    observer.observe(elem);
-  }
+const lined = document.querySelector(".focus-tl-pointer");
+const qline = document.querySelector(".qline");
+const sect = document.querySelector("#focus-tl");
+const sect2 = document.querySelector("#whoWeare");
+let options = {
+  root: sect,
 }
+let option2 = {
+  root: sect2,
+}
+const lineobserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+    else{
+      entry.target.classList.remove("active");
+    }
+  }, options);
+});
+lineobserver.observe(lined);
+const lineobserver2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+    else{
+      entry.target.classList.remove("active");
+    }
+  }, option2);
+});
+lineobserver2.observe(qline);
+
+// end
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll("nav ul li a");
 window.onscroll = () => {
